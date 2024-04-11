a1aba55fd31759a0921ae168bbfe237cc9b347bf
dev02chandan <dev02chandan@gmail.com>
2024-04-11 13:30:37 +0530

    Streamlit (working) but video not getting displayed on the app

---
+++ src/app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from shutil import copyfile
+from object_counting import process_video_and_count
+import tempfile
+import os
+
+# App title
+st.title('Object Detection and Counting with YOLOv8')
+
+
+
+# Video file uploader
+uploaded_video = st.file_uploader("Upload a mp4 video...", type=["mp4"])
+
+# Global classNames for use across scripts
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+# Selection of objects to detect
+selected_classes = st.multiselect(
+    'Select object classes to count',
+
+    options=classNames,
+
+    default=["chair", "laptop"]
+)
+
+# Convert class names to class IDs
+class_ids = [classNames.index(cls) for cls in selected_classes if cls in classNames]
+
+if uploaded_video is not None and len(selected_classes) > 0:
+    with st.spinner('Processing...'):
+
+        # Save uploaded video to a temporary file
+        tfile = tempfile.NamedTemporaryFile(delete=False) 
+        tfile.write(uploaded_video.read())
+        video_path = tfile.name
+
+        # Process video
+        run_dir = "runs/temp"
+        os.makedirs(run_dir, exist_ok=True)
+        object_counts, output_video_path = process_video_and_count(video_path, 'yolov8m.pt', class_ids, run_dir)
+        
+        video_file = open(output_video_path, 'rb')
+        video_bytes = video_file.read()
+
+        # Display results
+        st.video(video_bytes, format="video/mp4")
+
+        st.write(f"Object counts: {object_counts}")
+
