dfea8ccffc99d638233d6fe175c7ea26e26e0e02
dev02chandan <dev02chandan@gmail.com>
2024-05-02 11:25:00 +0530

    Tested on new set of classes

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from shutil import copyfile
-from object_counting import process_video_and_count
+from object_counting import process_video_and_count, process_image_and_count
 import tempfile
 import os
 
@@ -8,53 +8,46 @@ import os
 st.image("videos/logo.png", use_column_width=False)
 st.title('Object Detection and Counting')
 
+# Option to select input type
+input_type = st.radio("Select the input type:", ("Video", "Image"))
 
-
-# Video file uploader
-uploaded_video = st.file_uploader("Upload a mp4 video...", type=["mp4"])
-
-# Global classNames for use across scripts
-# classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-#               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-#               "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-#               "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-#               "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-#               "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-#               "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-#               "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-#               "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-#               "teddy bear", "hair drier", "toothbrush"
-#               ]
+# File uploader based on input type
+if input_type == "Video":
+    uploaded_file = st.file_uploader("Upload a mp4 video...", type=["mp4"])
+    file_type = "video"
+elif input_type == "Image":
+    uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "png"])
+    file_type = "image"
 
 classNames = ["cup", "cutter", "fork", "knife", "painting", "pan", "plant", "plate", 'scissor', 'spoon']
 
 # Selection of objects to detect
 selected_classes = st.multiselect(
     'Select object classes to count',
-
     options=classNames,
-
     default=["cup", "cutter"]
 )
 
 # Convert class names to class IDs
 class_ids = [classNames.index(cls) for cls in selected_classes if cls in classNames]
 
-if uploaded_video is not None and len(selected_classes) > 0:
+if uploaded_file is not None and len(selected_classes) > 0:
     with st.spinner('Processing...'):
+        # Save uploaded file to a temporary file
+        tfile = tempfile.NamedTemporaryFile(delete=False, suffix=f'.{uploaded_file.type.split("/")[-1]}')
+        tfile.write(uploaded_file.getvalue())
+        file_path = tfile.name
 
-        # Save uploaded video to a temporary file
-        tfile = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')  # Ensure suffix matches file type
-        tfile.write(uploaded_video.getvalue())  # Use getvalue() to read the content of the uploaded file
-        video_path = tfile.name
-
-        # Process video
+        # Process file based on type
         run_dir = "runs/temp"
         os.makedirs(run_dir, exist_ok=True)
-        object_counts, output_video_path = process_video_and_count(video_path, '11classes_trained.pt', class_ids, run_dir)
 
-        st.video(output_video_path)
+        if file_type == "video":
+            object_counts, output_path = process_video_and_count(file_path, '11classes_trained.pt', class_ids, run_dir)
+            st.video(output_path)
+        elif file_type == "image":
+            object_counts, output_path = process_image_and_count(file_path, '11classes_trained.pt', class_ids, run_dir)
+            st.image(output_path)
 
         # Display object counts
         st.write(f"Object counts: {object_counts}")
-
